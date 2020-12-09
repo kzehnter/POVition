3487ec046d7a7d2ba70241753f38bf4a97748c86
decimo1 <konstantinzehnter@protonmail.com>
2020-12-09 13:28:07 +0100

    No-VR Scene with swap-mechanic

---
+++ Assets/Scripts/MT/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Manages Movement of player object
+ *  J - K for turning
+ *  W - S for moving
+ *  Adjust speed in Inspector
+ */
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.J)) transform.Rotate(0,-3,0);
+        if (Input.GetKey(KeyCode.K)) transform.Rotate(0,3,0);
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed));
+    }
+}
---
+++ Assets/Scripts/MT/Swap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  Swaps positions of player and goal and adjusts their height
+ */
+public class Swap : MonoBehaviour
+{
+    public GameObject goalCube;
+    private Vector3 swap;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            swap = goalCube.transform.position;
+            swap.y += 0.5f;
+            goalCube.transform.position = transform.position;
+            goalCube.transform.position -= new Vector3(0,0.5f,0);
+            transform.position = swap;
+        }
+    }
+}
