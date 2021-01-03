fc7b41a7fdec08698204819b49d636eba47abdaf
decimo1 <konstantinzehnter@protonmail.com>
2021-01-03 14:18:38 +0100

    Timer script, count script, Prefabs for cubes

---
+++ Assets/Scripts/VR/SwapCubeAction_Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCubeAction_Timer : SwapCubeAction
+{
+    [SerializeField]
+    private int delayTime;
+
+    IEnumerator waiter(Transform target)
+    {
+        performSwap(target);
+        yield return new WaitForSeconds(delayTime);
+        performSwap(target);
+    }
+    
+    public override void performAction(Transform target)
+    {
+        StartCoroutine(waiter(target));
+    }
+}
