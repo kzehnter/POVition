668ae4286d04d66964aac892ed20faedfdfd33f8
decimo1 <konstantinzehnter@protonmail.com>
2021-01-04 01:45:50 +0100

    MT movement, organization, not done

---
+++ Assets/Scripts/MT/MTPlayerAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.Extras;
+
+/** MT Variant of PlayerAction. 
+ *
+ *  @author Eduard
+ *  adjusted by Konstantin
+ *
+ */
+public class MTPlayerAction : MonoBehaviour
+{
+    /** cooldown time for cube teleport */
+    [SerializeField]
+    private int swapCooldown;
+    private int _swapCooldown = 0;
+
+    private SteamVR_LaserPointer laserPointer;
+
+    /** adds callback to laser pointer click event */
+    private void Awake()
+    {
+        laserPointer = GetComponentInChildren<SteamVR_LaserPointer>();
+        laserPointer.PointerClick += OnPointerClick;
+    }
+
+    /** removes all event callbacks */
+    private void OnDestroy()
+    {
+        laserPointer.PointerClick -= OnPointerClick;
+    }
+
+    /** updates cooldown */
+    void FixedUpdate()
+    {
+        if (_swapCooldown > 0)
+        {
+            _swapCooldown--;
+        }
+            
+    }
+
+    /** Event callback. Triggers SwapCube interaction via laser pointer click  */
+    private void OnPointerClick(object sender, PointerEventArgs args)
+    {
+        if (args.target.tag == "SwapCube" && _swapCooldown == 0)
+        {
+            args.target.GetComponent<SwapCubeAction>().performAction(transform);
+            _swapCooldown = swapCooldown;
+        }
+    }
+}
--- Assets/Scripts/MT/PlayerMovement.cs
+++ Assets/Scripts/MT/PlayerMovement.cs
@@ -5,26 +5,47 @@ using UnityEngine;
 /** Manages Movement of player object.
  *  
  *  @author Konstantin
- *  @date 13.12.20
  *  
- *  J - K for turning, W - S for moving,
- *  adjust speed in Inspector
+ *  Mouse for turning, W - S for moving,
+ *  adjust movement in Inspector
  */
 public class PlayerMovement : MonoBehaviour
 {
     /** Movement speed for player.
-     *  Adjustable in Inspector,
-     *  normally at 0.05
+     *  2-5 realistic
      */
     public float speed;
+    /** Main Camera, for vertical adjustment.
+     */
+    public Camera camera;
+    /** Instead of rigidbody for more player options.
+     */
+    public CharacterController player;
+    /** Rotation speed, 2 is reasonable.
+     */
+    public float xRotSpeed;
+    /** Rotation speed, 2 is reasonable.
+     */
+    public float yRotSpeed;
+    /** Tracks vertical camera position.
+     */
+    private float yRot = 0;
+
 
     /** Movement at every Tick.
      *  Forwards/Backwards with transform.Translate
      */
     void Update()
     {
-        if (Input.GetKey(KeyCode.J)) transform.Rotate(0,-3,0);
-        if (Input.GetKey(KeyCode.K)) transform.Rotate(0,3,0);
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*speed, 0f, Input.GetAxis("Vertical")*speed));
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        yRot -= mouseY*yRotSpeed;
+        camera.transform.localRotation = Quaternion.Euler(yRot, 0, 0);
+        transform.Rotate(0, mouseX*xRotSpeed, 0); 
+        
+        // lock mouse to game window
+        if (Input.GetMouseButtonDown(0)) { Cursor.lockState = CursorLockMode.Locked; }
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        player.Move((transform.rotation * move.normalized * speed) * Time.deltaTime);
     }
 }
