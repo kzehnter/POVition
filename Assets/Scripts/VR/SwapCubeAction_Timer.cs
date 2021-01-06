55cf832e8e2eb38717531e3416ec781010bea41f
decimo1 <konstantinzehnter@protonmail.com>
2021-01-06 15:32:38 +0100

    changed scene names, Timer cube done

--- Assets/Scripts/VR/SwapCubeAction_Timer.cs
+++ Assets/Scripts/VR/SwapCubeAction_Timer.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/** Timercube actions.
+ *
+ *  @author Konstantin
+ */
 public class SwapCubeAction_Timer : SwapCubeAction
 {
     [SerializeField]
+    /** Time in seconds between swaps.
+     */
     private int delayTime;
 
-    IEnumerator waiter()
+    /** Waits delayTime and swaps.
+     */
+    IEnumerator waiter(Transform target)
     { 
-        yield return new WaitForSeconds(delayTime);   
+        yield return new WaitForSeconds(delayTime);
+        performSwap(target);   
     }
     
+    /** Swap, start waiter().
+     *  Startet by OnPointerClick in PlayerAction
+     */
     public override void performAction(Transform target)
     {
         performSwap(target);
-        StartCoroutine(waiter());
-        performSwap(target);
+        StartCoroutine(waiter(target));
     }
 }
