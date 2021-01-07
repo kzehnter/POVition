a31ca1ef555f48339ff5db824957d691258228fe
s0566430 <s0566430+github_desktop@htw-berlin.com>
2021-01-07 20:26:35 +0100

    added button click functionality

--- Assets/Scripts/VR/VR_InputModule.cs
+++ Assets/Scripts/VR/VR_InputModule.cs
@@ -46,11 +46,33 @@ public class VR_InputModule : BaseInputModule
 
     private void ProcessPress(PointerEventData data)
     {
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
 
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(m_CurrentObject, data, ExecuteEvents.pointerDownHandler);
+
+        if (newPointerPress == null)
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+
+        data.pressPosition = data.position;
+        data.pointerPress = newPointerPress;
+        data.rawPointerPress = m_CurrentObject;
     }
 
     private void ProcessRelease(PointerEventData data)
     {
+        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
 
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+
+        if (data.pointerPress == pointerUpHandler)
+        {
+            ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
+        }
+
+        eventSystem.SetSelectedGameObject(null);
+
+        data.pressPosition = Vector2.zero;
+        data.pointerPress = null;
+        data.rawPointerPress = null;
     }
 }
